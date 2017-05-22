0c846e8f6a2358d8bb97c28cf00c4a5374193005
jeanelbicho3 <abarcajean@gmail.com>
2017-05-21 21:48:27 -0600

    Puntajes para Usuarios
    Commiting

---
+++ ConoceCR/app/src/main/java/com/example/walter/conocecr/Puntaje.java
@@ -0,0 +1,31 @@
+package com.example.walter.conocecr;
+
+import android.widget.TextView;
+
+/**
+ * Created by Jean Abarca on 21-May-17.
+ */
+
+public class Puntaje {
+     TextView resultado;
+     String urlconexion = "http://www.nacion.com/rss/latest/?contentType=NWS";
+     ObtenerWebService hiloconexion = new ObtenerWebService(urlconexion);
+
+
+    // otro ejemplo:  http://www.elpais.com/rss/feed.html?feedId=1022
+    TextView textView;
+    //ObtenerWebService hiloconexion;
+
+
+    public void metodoMandatRequest(){
+
+        this.hiloconexion.execute();
+
+    //hiloconexion = new ObtenerWebService;
+
+    //hiloconexion.execute("haha","hoho");
+        //
+        //
+        }
+
+}
