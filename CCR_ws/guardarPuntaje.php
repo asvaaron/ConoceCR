77d8e178be30f238f680eb337b28ca25a2e37802
jeanelbicho3 <abarcajean@gmail.com>
2017-06-03 06:20:20 -0600

    Cambios a Guardar Puntajes
    klk

--- CCR_ws/guardarPuntaje.php
+++ CCR_ws/guardarPuntaje.php
@@ -2,9 +2,15 @@
 	// Include confi.php
 	include_once('confi.php');
 
-  $sql = "INSERT INTO 'puntajes' ('id_puntaje', 'usuario', 'puntaje', 'fecha')
-  VALUES ('001','John', '50', '11/20/2017')";
+  $sql = "INSERT INTO puntajes (id_puntaje, usuario, puntaje, fecha)
+  VALUES ('1','1', '50', '9999-11-30 23:58:58')";
 
   $qur = mysql_query($sql);
+
+	if ($qur === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 @mysql_close($conn);
